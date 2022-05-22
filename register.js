78e3afe03e2b39e98d6e7bdb4a2c7245be281286
Arun <102291925+Aruncopz@users.noreply.github.com>
2022-05-22 15:55:00 +0530

    Add files via upload

--- register.js
+++ register.js
@@ -11,14 +11,41 @@ form.addEventListener("submit" (event) => {
     localStorage.setItem("name");
     localStorage.setItem("designation");
     localStorage.setItem("email");
-
-
-
-
-
-
-
-
-
-
 })
+
+function registration() {
+
+    var userId = document.getElementById("t1").value;
+    var name = document.getElementById("t2").value;
+    var designation = document.getElementById("t3").value;
+    var email = document.getElementById("t4").value;
+
+
+    //email id expression code
+    var pwd_expression = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
+    var letters = /^[A-Za-z]+$/;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (userId == '') {
+        alert('Please enter your userId');
+    } else if (!letters.test(name)) {
+        alert('Name field required only alphabet characters');
+    } else if (name == '') {
+        alert('Please enter your user name');
+    } else if (designation == '') {
+        alert('Please enter the user designation.');
+    } else if (!letters.test(uname)) {
+        alert('designation field required only alphabet characters');
+    } else if (email == '') {
+        alert('Please enter email');
+    } else if (!filter.test(email)) {
+        alert('Invalid email');
+    }
+}
+
+function clearFunc() {
+    document.getElementById("t1").value = "";
+    document.getElementById("t2").value = "";
+    document.getElementById("t3").value = "";
+    document.getElementById("t4").value = "";
+}
